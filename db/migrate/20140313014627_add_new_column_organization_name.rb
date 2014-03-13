b171262c89091e07648b67739a245b4d97b6d985
Mar Angel Nepomuceno <mar_22183@yahoo.com>
2014-03-13 09:47:42 +0800

    new column under suplimentary details

---
+++ db/migrate/20140313014627_add_new_column_organization_name.rb
@@ -0,0 +1,8 @@
+class AddNewColumnOrganizationName < ActiveRecord::Migration
+  def up
+    add_column :supplemental_details, :organization_name, :string
+
+  end
+
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140311072556) do
+ActiveRecord::Schema.define(:version => 20140313014627) do
 
   create_table "education_details", :force => true do |t|
     t.string   "last_school_name"
