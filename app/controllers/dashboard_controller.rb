0117af073002546ebb6d1e99a035adc3b4497069
Paulo Mendoza <paupau7769@hotmail.com>
2014-05-15 23:41:40 +0800

    read status to id order

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,7 +7,7 @@ class DashboardController < ApplicationController
       temp_status_chart = OnlineApplicationForm.where('status != 90').count(:group => 'status')
       @status_chart = {}
       temp_status_chart.each_pair {|status_id, value| @status_chart.merge!(OnlineApplicationForm.get_status(status_id) => value)}
-      @applicants = OnlineApplicationForm.where('status != 90').order('read_status ASC').order('composite_updated_at DESC')
+      @applicants = OnlineApplicationForm.where('status != 90').order('id DESC').order('composite_updated_at DESC')
     elsif (params['status'].present? && (params['status'].to_i % 10 == 0))
       temp_status_chart = OnlineApplicationForm.where(:status => params['status'].to_i).count(:group => 'substatus')
       @status_chart = {}
@@ -15,7 +15,7 @@ class DashboardController < ApplicationController
         status = status_id.nil? ? 'No Status' : OnlineApplicationForm.get_status(status_id)
         @status_chart.merge!(status => value)
       end
-      @applicants = OnlineApplicationForm.where(status: params['status'].to_i).order('read_status ASC').order('composite_updated_at DESC')
+      @applicants = OnlineApplicationForm.where(status: params['status'].to_i).order('id DESC').order('composite_updated_at DESC')
     end
   end
 
