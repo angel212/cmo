0559cdcbe638f67096197b00b1ee07e021bf39fd
Mar Angel Nepomuceno <mar_22183@yahoo.com>
2014-05-27 08:48:56 +0800

    try_update

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,8 +1,12 @@
+
 $(document).ready(function (e) {
     $('#page-title').text("2ND ASIAN BUSINESS CONFERENCE REGISTRATION");
     $('#application-instructions').hide();
+
+
 })
 
 function showApplicationInstructions() {
     $('#application-instructions').show();
 }
+
