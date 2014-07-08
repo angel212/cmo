23e8afb8bd37b33a67eab3e339190cd7c3cd1fa1
Mar Angel Nepomuceno <mar_22183@yahoo.com>
2014-07-08 10:18:39 +0800

    first try

--- app/mailers/abc_registration_mailer.rb
+++ app/mailers/abc_registration_mailer.rb
@@ -3,6 +3,6 @@ class AbcRegistrationMailer < ActionMailer::Base
 
   def registration_alert(online_application_form)
     @online_application_form = online_application_form
-    mail(to: ['2nd.abc@aim.edu', 'adesagun@aim.edu', 'nmendoza@aim.edu'], subject: "Registration Received From #{@online_application_form.personal_detail.first_name} #{@online_application_form.personal_detail.last_name}")
+    #mail(to: ['2nd.abc@aim.edu', 'adesagun@aim.edu', 'nmendoza@aim.edu'], subject: "Registration Received From #{@online_application_form.personal_detail.first_name} #{@online_application_form.personal_detail.last_name}")
   end
 end
