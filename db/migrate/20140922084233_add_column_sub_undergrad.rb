d759bc6d9b4c754119721cd75903c0355edee394
Mar Angel Nepomuceno <mar_22183@yahoo.com>
2014-10-09 10:34:44 +0800

    new test2

--- db/migrate/20140922084233_add_column_sub_undergrad.rb
+++ db/migrate/20140922084233_add_column_sub_undergrad.rb
@@ -1,6 +1,6 @@
 class AddColumnSubUndergrad < ActiveRecord::Migration
   def up
-    add_column :undergraduate_majors , :sub_undergrad, :sting
+    add_column :undergraduate_majors , :sub_undergrad, :string
   end
 
 end
