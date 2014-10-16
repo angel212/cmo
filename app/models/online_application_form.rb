89bd2b463704685a89e83bdb4969f1b71882787f
Mar Angel Nepomuceno <mar_22183@yahoo.com>
2014-10-16 16:41:36 +0800

    asda

--- app/models/online_application_form.rb
+++ app/models/online_application_form.rb
@@ -1,5 +1,5 @@
 class OnlineApplicationForm < ActiveRecord::Base
-  attr_accessible :composite_updated_at, :education_detail_id, :employment_detail_id, :personal_detail_id, :preference_id, :supporting_document_id, :user_id, :status , :password
+  attr_accessible :composite_updated_at, :education_detail_id, :employment_detail_id, :personal_detail_id, :preference_id, :supporting_document_id, :user_id, :status , :pass
   belongs_to :personal_detail
   belongs_to :employment_detail
   belongs_to :education_detail
